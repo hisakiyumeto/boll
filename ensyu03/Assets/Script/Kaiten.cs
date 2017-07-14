19a6bea44997e6f236d1acce5964a28f034beead
karuto <0up24913725378f@gmail.com>
2017-07-14 15:33:37 +0900

    no message

---
+++ ensyu03/Assets/Script/Kaiten.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kaiten : MonoBehaviour {
+
+    bool direction;
+
+    void Start()
+    {
+        direction = true;
+
+    }
+
+    void Update()
+    {
+
+        if (direction)
+        {
+            if (transform.eulerAngles.z < 365)
+            {
+                transform.Rotate(0, 0, 10 * Time.deltaTime);
+
+            }
+            if (Mathf.Round(transform.eulerAngles.z) == 365)
+            {
+                direction = !direction;
+            }
+        }
+
+        if (!direction)
+        {
+            transform.Rotate(0, 0, -10 * Time.deltaTime);
+
+            if (Mathf.Round(transform.eulerAngles.z) == 0)
+            {
+                direction = !direction;
+            }
+        }
+
+    }
+}
---
+++ ensyu03/Assets/Script/StageSelectManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelectManager : MonoBehaviour {
+
+    public GameObject[] stageButtons;
+
+	// Use this for initialization
+	void Start () {
+        int clearStageNo = PlayerPrefs.GetInt("CLEAR", 0);
+        for (int i = 0; i <= stageButtons.GetUpperBound(0); i++)
+            {
+            bool b;
+            if (clearStageNo < i)
+            {
+                b = false;
+            }
+            else
+            {
+                b = true;
+            }
+            stageButtons[i].GetComponent<Button>().interactable = b;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void PushStageSelectButton(int stageNo)
+    {
+        SceneManager.LoadScene("PuzzleScene" + stageNo);
+    }
+}
