c3d83980a6c0961c064f4c125ecc8cbc66969fbd
DA\nwuser <yumeto0216@gmail.com>
2017-07-17 16:16:58 +0900

    no message

---
+++ 結合ensyu03/Assets/Script/BotanIdou.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BotanIdou : MonoBehaviour {
+
+    
+    // Use this for initialization
+    void Start () {
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    public void StageSelectScene()
+    {
+        SceneManager.LoadScene("StageSelectScene");
+    }
+    public void StageSelectScene2()
+    {
+        SceneManager.LoadScene("StageSelectScene2");
+    }
+}
