8ef6c51f5410333cbb593775ccc67269a390cb5a
karuto <0up24913725378f@gmail.com>
2017-10-13 14:15:59 +0900

    no message

---
+++ AFFF/Assets/Script/Bar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar : MonoBehaviour {
+    int x;
+
+    private Vector3 initialPosition;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Sin(Time.time) * 4.0f + initialPosition.x, initialPosition.y, initialPosition.z);
+    }
+}
