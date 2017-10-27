c1de01e0d0999093eec33d4e3798f3ac6d5e1e7b
karuto <0up24913725378f@gmail.com>
2017-10-27 15:20:57 +0900

    no message

---
+++ 更新版/Assets/Script/Kaiten2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kaiten2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}    float direction = 80.0f;
+        float speed = 1.0f;
+        Vector2 v;
+	  
+	// Update is called once per frame
+	void Update () {
+    
+      v.x = Mathf.Cos(Mathf.Deg2Rad * direction) * speed;
+        v.y = Mathf.Sin(Mathf.Deg2Rad * direction) * speed;
+
+        Rigidbody2D rd = GetComponent<Rigidbody2D>();
+        rd.velocity = v;
+    }  // 40度方向に5の速度で移動する
+      
+        
+}
